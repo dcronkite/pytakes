f898f4cb2619947fb884d0f8903b7565c30b8c6a
dcronkite <dcronkite@gmail.com>
2015-09-29 15:32:49 -0700

    Added options for concept miner version 3 (ngrams).

--- src/automate_run.py
+++ src/automate_run.py
@@ -72,15 +72,8 @@ def getNumberOfFiles(batches):
             return numberOfFiles, batchesPerFile
 
 
-def createBatchFile(output_dir, number, dictionary_table, negation_table,
-                    negation_variation, document_table, destination_table,
-                    batch_size, batch_start, batch_end, server, database,
-                    max_intervening_terms, max_length_of_search,
-                    valence, regex_variation, word_order, meta_labels):
-    optional = '\n'.join(['{}={}'.format(x, y) for x, y in [('--valence', valence),
-                                                            ('--regex-variation', regex_variation),
-                                                            ('--word-order', word_order)]
-                          if y])
+def createBatchFile(output_dir, number, document_table, destination_table,
+                    batch_size, batch_start, batch_end, driver, server, database, meta_labels, cm_options):
     meta_labels = '\n'.join(meta_labels) if meta_labels else '\n'.join(('ft_id', 'chsid'))
     with open(os.path.join(output_dir, 'ctakes-batch' + str(number) + '.bat'), 'w') as out:
         out.write(
@@ -96,28 +89,25 @@ echo Failed.
 )
 pause''' % (number, number, number, number, batch_start))
 
+    options = '\n'.join(['--{}={}'.format(x, y) for x, y in cm_options if y])
+
     with open(os.path.join(output_dir, 'ctakes-batch' + str(number) + '.conf'), 'w') as out:
         out.write(
-            r'''--server=%s
---database=%s
---term-table=%s
---negation-table=%s
---negation-variation=%s
---document-table=%s
+            r'''--driver={}
+--server={}
+--database={}
+--document-table={}
 --meta-labels
-%s
+{}
 --text-labels=note_text
---destination-table=%s_pre
---concept-miner=2
---max-intervening-terms=%d
---max-length-of-search=%d
-%s
+--destination-table={}_pre
+{}
 --batch-mode=ft_id
---batch-size=%d
+--batch-size={}
 --batch-number
-%d
-%d''' % (server, database, dictionary_table, negation_table, negation_variation, document_table, meta_labels,
-         destination_table, max_intervening_terms, max_length_of_search, optional, batch_size, batch_start, batch_end))
+{}
+{}'''.format(driver, server, database, document_table, meta_labels, destination_table,
+             options, batch_size, batch_start, batch_end))
     return
 
 
@@ -141,7 +131,7 @@ The log file is included for debugging.
 ''' % (numberOfFiles, destination_table))
 
 
-def createPostProcessBatch(pp_dir, destination_table, negation_table, negation_variation,
+def createPostProcessBatch(pp_dir, destination_table, negation_table, negation_variation, driver,
                            server, database):
     with open(os.path.join(pp_dir, 'postprocess.bat'), 'w') as out:
         out.write(
@@ -150,32 +140,30 @@ pause
 ''')
     with open(os.path.join(pp_dir, 'postprocess.conf'), 'w') as out:
         out.write(
-            r'''--server=%s
+            r'''--driver=%s
+--server=%s
 --database=%s
 --input-table=%s_pre
 --output-table=%s
 --negation-table=%s
 --negation-variation=%s
 --input-column=captured
-''' % (server, database, destination_table, destination_table, negation_table, negation_variation))
+''' % (driver, server, database, destination_table, destination_table, negation_table, negation_variation))
 
 
 def main(dbi,
-         dictionary_table,
-         negation_table,
-         negation_variation,
+         cm_options,
+         concept_miner,
          document_table,
          output_dir,
          destination_table,
+         driver,
          server,
          database,
-         max_intervening_terms,
-         max_length_of_search,
-         valence,
-         regex_variation,
-         word_order,
          meta_labels,
-         recipients):
+         recipients,
+         negation_table, negation_variation):
+
     count = getDocumentCount(dbi, document_table)
     print 'Found %d documents in %s.' % (count, document_table)
     batchSize, batchCount = getSizeOfBatches(count)
@@ -190,52 +178,88 @@ def main(dbi,
     batch_start = 1
     for i in range(1, numberOfFiles + 1):
         batch_end = batch_start + batchesPerFile
-        createBatchFile(output_dir, i, dictionary_table, negation_table,
-                        negation_variation, document_table, destination_table,
-                        batchSize, batch_start, batch_end, server, database, max_intervening_terms,
-                        max_length_of_search, valence, regex_variation, word_order, meta_labels)
+        createBatchFile(output_dir, i, document_table, destination_table,
+                        batchSize, batch_start, batch_end, server, database, meta_labels,
+                        cm_options)
         batch_start = batch_end
 
     createEmailFile(output_dir, numberOfFiles, destination_table, recipients)
 
     postprocess_dir = os.path.join(output_dir, 'post')
     mkdir_p(postprocess_dir)
-    createPostProcessBatch(postprocess_dir, destination_table, negation_table, negation_variation,
-                           server, database)
+    if concept_miner == 2:
+        createPostProcessBatch(postprocess_dir, destination_table, negation_table, negation_variation,
+                               driver, server, database)
     logging.info('Completed.')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+    parser.add_argument('--driver', default='SQL Server', help='Driver to connect with.')
     parser.add_argument('-s', '--server', default='ghrinlp', help='Database server to use.')
     parser.add_argument('-d', '--database', default='nlpdev', help='Database to use.')
-    parser.add_argument('--dictionary-table', help='Term table/dictionary table as input.')
-    parser.add_argument('--negation-table', help='Negation/status table with (negex,status,direction).')
-    parser.add_argument('--negation-variation', help='Negation variation [0-3].')
     parser.add_argument('--document-table', help='Table of input documents.')
     parser.add_argument('--output-dir', help='Destination directory.')
     parser.add_argument('--destination-table', help='Output table, to be created.')
+
+    parser.add_argument('--concept-miner', default=2, type=int, required=False,
+                        help='Version of concept miner to use.')
+    # concept miner 2
     parser.add_argument('--max-intervening-terms', default=1, type=int,
                         help='Max number of terms that can occur between searched for terms.')
     parser.add_argument('--max-length-of-search', required=False, default=3, type=int,
                         help='Max number of words in which to look for the next term.')
-
     parser.add_argument('--valence', required=False, default=None, type=int)
-    parser.add_argument('--meta-labels', nargs='+', help='Extra identifying labels to include in output.')
     parser.add_argument('--regex-variation', required=False, default=None, type=int)
     parser.add_argument('--word-order', required=False, default=None, type=int)
+    parser.add_argument('--dictionary-table', help='Term table/dictionary table as input.')
+    parser.add_argument('--negation-table', help='Negation/status table with (negex,status,direction).')
+    parser.add_argument('--negation-variation', help='Negation variation [0-3].')
+
+    # concept miner 3
+    parser.add_argument('--stopwords', required=False, default=None, nargs='+',
+                        help='Stopwords to include.')
+    parser.add_argument('--number-normalization', required=False, action='store_true', default=False,
+                        help='Whether or not to normalize all numbers to a default value.')
+    parser.add_argument('--stopword-tables', required=False, default=None, nargs='+',
+                        help='Tables containing relevant stopwords. NYI')
+
+    parser.add_argument('--meta-labels', nargs='+', help='Extra identifying labels to include in output.')
+
     parser.add_argument('-v', '--verbosity', type=int, default=2, help='Verbosity of log output.')
     parser.add_argument('--recipients', required=True, default=None, nargs='+',
-                                      help='In format of "name,email@address"')
+                        help='In format of "name,email@address"')
     args = parser.parse_args()
 
     loglevel = mylogger.resolveVerbosity(args.verbosity)
     logging.config.dictConfig(mylogger.setup(name='automate_run', loglevel=loglevel))
 
-    dbi = dbInterface(driver='SQL Server', server=args.server, database=args.database, loglevel=loglevel)
+    dbi = dbInterface(driver=args.driver, server=args.server, database=args.database, loglevel=loglevel)
+
+    if args.concept_miner == 2:
+        cm_options = [
+            ('concept-miner', 2),
+            ('term-table', args.dictionary_table),
+            ('negation-table', args.negation_table),
+            ('negation-variation', args.negation_variation),
+            ('max-intervening-terms', args.max_intervening_terms),
+            ('max-length-of-search', args.max_length_of_search),
+            ('valence', args.valence),
+            ('regex-variation', args.regex_variation),
+            ('word-order', args.word_order)
+        ]
+    elif args.concept_miner == 3:
+        cm_options = [
+            ('concept-miner', 3),
+            ('stopwords', args.stopwords),
+            ('number-normalization', args.number_normalization),
+            ('stopword-tables', args.stopword_tables)
+        ]
+    else:
+        raise ValueError('Invalid argument for concept miner.')
 
     try:
-        main(dbi, **get_valid_args(main, vars(args)))
+        main(dbi, cm_options, **get_valid_args(main, vars(args)))
     except Exception as e:
         logging.exception(e)
         logging.error('Process terminated with errors.')
