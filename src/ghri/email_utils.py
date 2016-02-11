875bf29e757a6d678278812cca6e31778667119e
dcronkite <dcronkite@gmail.com>
2016-02-11 11:55:57 -0800

    Don't require sender/server being specified.

--- src/ghri/email_utils.py
+++ src/ghri/email_utils.py
@@ -136,6 +136,6 @@ if __name__ == '__main__':  # if run from cmd line, not if imported
     parser.add_argument('-t', '--text', default=[], nargs='+', help='Text for email content.')
     parser.add_argument('-r', '--recipients', nargs='+', default=[], help='Recipients\' name,address.')
     parser.add_argument('--sender', default=None, help='Sender name,address.')
-    parser.add_argument('--server', required=True, help='Mail server path.')
+    parser.add_argument('--server', default=None, help='Mail server path.')
     args = parser.parse_args()
     main(**vars(args))  # set up e-mail message and send
