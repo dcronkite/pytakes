d70bba5860ba6c006ee4bc9700fab04cd3516757
dcronkite <dcronkite@gmail.com>
2023-07-24 12:13:27 -0700

    Uncommenting tests

--- tests/test_keywords.py
+++ tests/test_keywords.py
@@ -7,9 +7,9 @@ from pytakes.iolib.txt import TxtDictionary
 
 @pytest.mark.parametrize('text, rules, kwargs, expected', [
     # concepts will be left alone and compiled without escaping
-    # ('Forest for the trees. A tree?', ['tree'], {'regex_variation': -1}, ['tree', 'tree']),
-    # ('Forest for the trees. A tree?', ['tree'], {'regex_variation': 0}, ['trees', 'tree']),
-    # ('Do you always write such charming long letters to her?', ['letters?'], {'regex_variation': 0}, ['letters']),
+    ('Forest for the trees. A tree?', ['tree'], {'regex_variation': -1}, ['tree', 'tree']),
+    ('Forest for the trees. A tree?', ['tree'], {'regex_variation': 0}, ['trees', 'tree']),
+    ('Do you always write such charming long letters to her?', ['letters?'], {'regex_variation': 0}, ['letters']),
     ('charming long letters', ['letters?', r'charm\w+ letters?'], {'regex_variation': 0},
      ['charming long letters', 'letters']),
 ])
